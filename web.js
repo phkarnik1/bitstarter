ebb730aa62ebb12e14c495ea2a7d544cf8d402ee
Ubuntu <ubuntu@ip-172-31-39-192.us-west-2.compute.internal>
2014-01-11 07:51:15 +0000

    from index.html

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+  fs.readFile('index.html', function(err,data){
+    if(err) throw err;
+    console.log(data);
+    response.send(data);
+  });
   response.send('Hello World 2!');
 });
 
